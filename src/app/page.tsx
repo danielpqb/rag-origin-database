a3d8ddfe9e2e59611fc37b81ecef5c09984e174a
danielpqb <danielpqb@gmail.com>
2024-02-24 14:16:08 -0300

    feat: render all tables at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-gray-950 py-4">
       <ItemsTable />
+      <MonstersTable />
     </main>
   );
 }
